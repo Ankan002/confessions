74451eb055152bfc9a2e80561a8d4097c32b128d
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-20 17:06:30 +0530

    chore: :tada: project initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col">
+      <h1 className="text-3xl">
+        Welcome to Confessions
+      </h1>
+    </main>
+  )
+}
