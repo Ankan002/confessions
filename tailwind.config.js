fad4b2c9d11204d6e446f1e580a45a1421805ca9
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-28 12:24:35 +0530

    feat: :sparkles: add color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDarkYellow: "#F9BB75",
+        primaryLightYellow: "#FCE98F",
+        greenGrey: "#89BB9A",
+        cobaltBlue: "#A0F5F5",
+        primaryRed: "#FF2A65",
+        primaryDark: "#1C1B2B",
+        secondaryDark: "#10111A",
+        primaryLight: "#F4F6FA",
+        secondaryLight: "#F1F2F6",
+        primaryOrange: "#FC5830",
+        lightGrey: "#B5BAC7",
+        white: "#FFFFFF",
+        semiTransparent: "rgba(0, 0, 0, 0.6)",
+        primaryGreen: "#08CD92",
+      }
+    },
   },
   plugins: [],
 }
