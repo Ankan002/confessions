ac3c61e2247456e1d00f25eed5c47f437696ce39
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-31 19:41:58 +0530

    fix: :ambulance: fix conditional hook bug

--- components/elements/one-tap-provider/one-tap-provider.tsx
+++ components/elements/one-tap-provider/one-tap-provider.tsx
@@ -2,14 +2,14 @@
 import { useGoogleOneTapLogin } from "@react-oauth/google";
 import { ReactNode } from "react";
 import jwtDecode from "jwt-decode";
+import { useRecoilValue } from "recoil";
+import { authAtom } from "atoms";
 
 interface Props {
     children: ReactNode
 }
 
-const OneTapProvider = (props: Props) => {
-
-  const { children } = props;
+const OneTapLogin = () => {
 
   useGoogleOneTapLogin({
     onSuccess: async (credentialResponse) => {
@@ -22,9 +22,27 @@ const OneTapProvider = (props: Props) => {
     onError: () => {
         console.log("Login Failed!!");
     }
-  })  
+  });
+  
+  
+  return <></>;
+}
 
-  return <>{children}</>;
+const OneTapProvider = (props: Props) => {
+
+  const { children } = props;
+  const isAuthenticated = useRecoilValue<boolean>(authAtom);
+
+  return (
+    <>
+      {
+        !isAuthenticated && (
+          <OneTapLogin />
+        )
+      }
+      {children}
+    </>
+  );
 };
 
 export default OneTapProvider;
