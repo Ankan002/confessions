79596d0e9a95279d3dd85b0c617cd770e51698c0
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-02-12 10:27:26 +0530

    chore: :wrench: update navbar

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full px-4 py-2 flex items-center shadow-md sticky top-0 bg-primary-light dark:bg-primary-dark">
+      <div>
+        <h1 className="font-quicksand tracking-wider text-primary-dark dark:text-primary-light text-4xl">
+          Confessions
+        </h1>
+      </div>
+
+      <div>
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
+import { Navbar } from "@/components/navbar";
 import React from "react";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col bg-primary-light dark:bg-secondary-dark">
-      <h1 className="text-3xl text-primary-dark dark:text-primary-light">
-        Welcome to Confessions
-      </h1>
+    <main className="min-h-screen flex flex-col bg-primary-light dark:bg-primary-dark">
+      <Navbar />
     </main>
   )
 }
